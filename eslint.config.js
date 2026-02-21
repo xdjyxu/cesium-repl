144ef73812cdb5c601a57dfe2430f984cdb5e2d9
许家毓 <xdjyxu@163.com>
2026-02-21 10:15:02 +0800

    chore: 为 examples 目录配置 ESLint import 排序规则

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,26 @@ export default antfu({
   formatters: true,
   unocss: true,
   vue: true,
+}, {
+  files: ['examples/**/*.{js,ts,jsx,tsx}'],
+  rules: {
+    'no-console': 'off',
+    'perfectionist/sort-imports': ['error', {
+      customGroups: {
+        value: {
+          'cesium-first': ['^cesium$', '^Sandcastle$'],
+        },
+      },
+      groups: [
+        'cesium-first',
+        'type-import',
+        ['value-builtin', 'value-external'],
+        'type-internal',
+        'value-internal',
+        ['type-parent', 'type-sibling', 'type-index'],
+        ['value-parent', 'value-sibling', 'value-index'],
+        'unknown',
+      ],
+    }],
+  },
 })
